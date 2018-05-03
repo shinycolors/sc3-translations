c9aeb3ae96d0d1771f94daf2c21966f08444d18a
Jet Rico <jetriconew@gmail.com>
2018-05-03 11:48:38 +0800

    en_us updates, produce events

--- en_us/index.js
+++ en_us/index.js
@@ -1,8 +1,16 @@
 import names from './names.json'
-import produce from './produce.json'
+
+import produceCommon from './produce-common.json'
+import produceMano from './produce-mano.json'
+import produceHiori from './produce-hiori.json'
+import produceMeguru from './produce-meguru.json'
+
 import hioriEnd from './hiori-end.json'
 
 export default []
   .concat(names.messages)
-  .concat(produce.messages)
+  .concat(produceCommon.messages)
+  .concat(produceMano.messages)
+  .concat(produceHiori.messages)
+  .concat(produceMeguru.messages)
   .concat(hioriEnd.messages)
