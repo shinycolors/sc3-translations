6691b992397ead71d4b0ebfe6422f0ccda8235a6
Jet Rico <jetriconew@gmail.com>
2018-05-28 07:06:47 +0800

    #30 Kaho: Produce Start

--- en_us/index.js
+++ en_us/index.js
@@ -11,6 +11,8 @@ import hioriTrueEnd from './hiori/true-end.json'
 
 import juriProduceStart from './juri/produce-start.json'
 
+import kahoProduceStart from './kaho/produce-start.json'
+
 import manoProduceStart from './mano/produce-start.json'
 
 import natsuhaProduceStart from './natsuha/produce-start.json'
@@ -28,6 +30,7 @@ export default []
   .concat(hioriProduceStart.messages)
   .concat(hioriTrueEnd.messages)
   .concat(juriProduceStart.messages)
+  .concat(kahoProduceStart.messages)
   .concat(manoProduceStart.messages)
   .concat(natsuhaProduceStart.messages)
   .concat(rinzeIdolHonorStudent.messages)
