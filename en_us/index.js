021e58f3dd56f6388a45ed30bbfdffe288d866c7
Jet Rico <jetriconew@gmail.com>
2018-05-28 06:54:07 +0800

    #36 Juri: Produce Start

--- en_us/index.js
+++ en_us/index.js
@@ -2,7 +2,12 @@ import commonEtc from './common/etc.json'
 import commonNames from './common/names.json'
 import commonProduceStart from './common/produce-start.json'
 
+import chiyukiProduceStart from './chiyuki/produce-start.json'
+
+import hioriProduceStart from './hiori/produce-start.json'
 import hioriTrueEnd from './hiori/true-end.json'
+
+import juriProduceStart from './juri/produce-start.json'
 
 import manoProduceStart from './mano/produce-start.json'
 
@@ -12,6 +17,9 @@ export default []
   .concat(commonEtc.messages)
   .concat(commonNames.messages)
   .concat(commonProduceStart.messages)
+  .concat(chiyukiProduceStart.messages)
+  .concat(hioriProduceStart.messages)
   .concat(hioriTrueEnd.messages)
+  .concat(juriProduceStart.messages)
   .concat(manoProduceStart.messages)
   .concat(rinzeIdolHonorStudent.messages)
